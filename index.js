df66a2b14f2ce3f3a0087ad8b4e707ce2862ef2b
huang <183433307@qq.com>
2022-04-14 12:21:01 +0800

    测试service酱推送

--- index.js
+++ index.js
@@ -161,25 +161,26 @@ const checkIn = async () => {
       const getCheckInDaysRes = await getCheckInDays()
       console.log(`连续签到【${getCheckInDaysRes.continuousDay}】天  总签到天数【${getCheckInDaysRes.sumCount}】  掘金不停 签到不断💪`)
 
-      push_url = push_url + push_key + ".send?text=" + '掘金签到成功!' + "&desp=" + `签到成功+${checkInRes.data.incr_point}矿石，总矿石${checkInRes.data.sum_point}`+`连续签到【${getCheckInDaysRes.continuousDay}】天  总签到天数【${getCheckInDaysRes.sumCount}】  掘金不停 签到不断💪`
+      push_url = push_url + push_key + ".send?text=" + '掘金签到成功!' + "&desp=" + "掘金签到成功"
 
       // const push_service = await axios({ push_url, method: 'get' })
-      axios.get(push_url,{ params: '' }).then(res=>{
+      axios.get(push_url).then(res=>{
         console.log(res)
       })
       // 签到成功 去抽奖
       await draw()
     } else {
-      push_url = push_url + push_key + ".send?text=" + '今日掘金已签到' + "&desp=" + '今日掘金已签到'
+      push_url = push_url + push_key + ".send?text=" + '今日掘金已签到' + "&desp=" + "今日掘金已签到"
       // const push_service = await axios({ push_url, method: 'get' })
-      axios.get(push_url,{ params: '' }).then(res=>{
+
+      axios.get(push_url).then(res=>{
         console.log(res)
       })
       console.log('今日已经签到 ✅')
     }
 
   } catch (error) {
-    push_url = push_url + push_key + ".send?text=" + '签到失败!' + "&desp=" + '签到失败!'
+    push_url = push_url + push_key + ".send?text=" + '签到失败!' + "&desp=" + "签到失败!"
     // const push_service = await axios({ push_url, method: 'get' })
     axios.get(push_url,{ params: '' }).then(res=>{
       console.log(res)
